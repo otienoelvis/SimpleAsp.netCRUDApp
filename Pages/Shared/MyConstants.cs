bbda75ef795b7c299022edcba9fbf223dbff4abb
otienoelvis <sevileelvis@gmail.com>
2022-07-11 12:36:09 +0300

    Done

--- Pages/Shared/MyConstants.cs
+++ Pages/Shared/MyConstants.cs
@@ -2,5 +2,6 @@ namespace jokiso.Pages.Shared;
 
 public static class MyConstants
 {
-    public const string connectionString = "Server=localhost;Port=3306;Database=jokesapp;Uid=don;Pwd=12345678;connect timeout=100;default command timeout=200;";
+    // Used MySql database
+    public const string connectionString = "Server=localhost;Port=3306;Database={databaseName};Uid={username};Pwd={password};connect timeout=100;default command timeout=200;";
 }
